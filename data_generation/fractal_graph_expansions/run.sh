26fd6c018bbd621d45102d154d3ae2e8188a754c
Rayna Levinson <rlevinso@localhost.localdomain>
2020-02-17 12:39:20 -0500

    added mlperf_compliance as requirement, including fetched changes from upstream

--- data_generation/fractal_graph_expansions/run.sh
+++ data_generation/fractal_graph_expansions/run.sh
@@ -5,5 +5,5 @@ output_prefix=""
 num_row_multiplier=16
 num_col_multiplier=32
 
-python run_expansion.py --input_csv_file=$input_file --output_prefix=$output_prefix --num_row_multiplier=$num_row_multiplier --num_col_multiplier=$num_col_multiplier
+python3 run_expansion.py --input_csv_file=$input_file --output_prefix=$output_prefix --num_row_multiplier=$num_row_multiplier --num_col_multiplier=$num_col_multiplier
 # python post_process.py --output_prefix=$output_prefix --num_shards=$num_row_multiplier
