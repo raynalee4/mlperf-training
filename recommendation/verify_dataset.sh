a8772017fb17f86b6bfb4244af7db63b69651fa0
Rayna Levinson <rlevinso@localhost.localdomain>
2020-03-18 11:15:02 -0700

    recommendation mlperf tekton pipeline

--- recommendation/verify_dataset.sh
+++ recommendation/verify_dataset.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function get_checker {
     if [[ "$OSTYPE" == "darwin"* ]]; then
         checkmd5=md5
@@ -13,7 +15,7 @@ function verify_1m {
     # From: curl -O http://files.grouplens.org/datasets/movielens/ml-1m.zip.md5
     hash=<(echo "MD5 (ml-1m.zip) = c4d9eecfca2ab87c1945afe126590906")
     local checkmd5=$(get_checker)
-    if diff <($checkmd5 ml-1m.zip) $hash &> /dev/null
+    if diff $($checkmd5 ml-1m.zip) $hash &> /dev/null
     then
         echo "PASSED"
     else
@@ -26,7 +28,7 @@ function verify_20m {
     hash=<(echo "MD5 (ml-20m.zip) = cd245b17a1ae2cc31bb14903e1204af3")
     local checkmd5=$(get_checker)
 
-    if diff <($checkmd5 ml-20m.zip) $hash &> /dev/null
+    if diff $($checkmd5 ml-20m.zip) $hash &> /dev/null
     then
         echo "PASSED"
     else
