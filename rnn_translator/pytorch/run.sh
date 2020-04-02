fb3d4729afb64c41d0e80cda593f2cab6e714b05
Rayna Levinson <rlevinso@localhost.localdomain>
2020-04-02 12:34:40 -0700

    python ascii error fixes

--- rnn_translator/pytorch/run.sh
+++ rnn_translator/pytorch/run.sh
@@ -3,6 +3,8 @@ source scl_source enable rh-python36 devtoolset-7
 set -e
 
 DATASET_DIR='/output_dir'
+RESULTS_DIR='/results'
+SAVE_DIR="gnmt"
 
 SEED=${1:-"1"}
 TARGET=${2:-"24.00"}
@@ -12,4 +14,8 @@ TARGET=${2:-"24.00"}
 python train.py \
   --dataset-dir ${DATASET_DIR} \
   --seed $SEED \
-  --target-bleu $TARGET
+  --target-bleu $TARGET \
+  --save-all \
+  --results-dir $RESULTS_DIR \
+  --save $RESULTS_DIR/$SAVE_DIR \
+  --resume "/results/gnmt/checkpoint_epoch_000.pth"
